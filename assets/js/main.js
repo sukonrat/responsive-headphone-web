24cd1061680475170b1fa96580dfab71c116e122
sukonrat <48665087+sukonrat@users.noreply.github.com>
2021-09-03 16:50:16 +1000

    implemented footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,39 @@
 /* =================== SHOW MENU =================== */
-const navMenu = document.getElementById('nav_menu'),
-    navToggle = document.getElementById('nav_toggle'),
-    navClose = document.getElementById('nav_close')
+const navMenu = document.getElementById("nav_menu"),
+  navToggle = document.getElementById("nav_toggle"),
+  navClose = document.getElementById("nav_close");
 
-    /* ==== Menu show ====*/
-    /* Validate if constant exist */
-    if(navToggle) {
-        navToggle.addEventListener('click', () => {
-            navMenu.classList.add('show-menu')
-        })
-    }
+/* ==== Menu show ====*/
+/* Validate if constant exist */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
-        /* ==== Menu hidden ====*/   
-         /* Validate if constant exist */
-    if(navClose){
-        navClose.addEventListener('click', ()=> {
-            navMenu.classList.remove('show-menu')
-        })
-    }
+/* ==== Menu hidden ====*/
+/* Validate if constant exist */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /* =================== REMOVE MENU MOBILE=================== */
-    const navLink = document.querySelectorAll('.nav-link')
+const navLink = document.querySelectorAll(".nav-link");
 
 function linkAction() {
-    const navMenu = document.getElementById('nav_menu')
-    //when we click on each link, it remove show-menu
-    navMenu.classList.remove('show-menu')
+  const navMenu = document.getElementById("nav_menu");
+  //when we click on each link, it remove show-menu
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach( n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 /* =================== CHANgE BACKGROUND HEADER =================== */
-function scrollHeader () {
-    const header = document.getElementById('header')
+function scrollHeader() {
+  const header = document.getElementById("header");
 
-    //when scroll is greater than 50 viewpoint height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+  //when scroll is greater than 50 viewpoint height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
