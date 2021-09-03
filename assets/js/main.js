084c6ca558a90c13858f924d987d205b2e122587
sukonrat <48665087+sukonrat@users.noreply.github.com>
2021-09-03 17:41:15 +1000

    implemented scrollup feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,37 @@ function scrollHeader() {
   else header.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
+
+
+/* =================== SHOW SCROLL UP =================== */
+function scrollUp() {
+    const scrollUp = document.getElementById("scroll-up")
+
+    //when scroll is higher han 200 viewpoint height, add the show-scroll class to the header tag
+    if(this.scrollY >= 200 ) scrollUp.classList.add("show-scroll") ; else scrollUp.classList.remove("show-scroll")  
+}
+
+window.addEventListener("scroll", scrollUp);
+
+/* ===================  SCROLL SCETIONS ACTIVE LINK =================== */
+const sections = document.querySelectorAll("section[id]")
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link');
+        }else{
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link');
+
+        }
+    })
+}
+
+window.addEventListener("scroll",scrollActive)
